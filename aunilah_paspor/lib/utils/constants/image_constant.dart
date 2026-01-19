a63c42b1cae28d767c82eb64c8f2d2037d3ea99f
Naubill Ode <naubilode@gmail.com>
2026-01-19 22:42:42 +0800

    updated Aunillah MVVM

--- aunilah_paspor/lib/utils/constants/image_constant.dart
+++ aunilah_paspor/lib/utils/constants/image_constant.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ImageConstant {
-  static const String mainLogo = 'assets/image/logoAwal.png';
+  static const String mainLogo = 'assets/image/logo_putih.png';
+  static const String mainLogo2 = 'assets/image/logo_ungu.png';
 }
